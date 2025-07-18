683e48204eaeae11ac686e0b42015a50dd1dd046
Manoel Ívisson <manoelivisson4@hotmail.com>
2025-07-18 16:48:48 -0300

    Criando dicionário

--- app.py
+++ app.py
@@ -2,14 +2,19 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+coordenadas = {}
+ponto = 1
 
 @app.route("/", methods=["GET"])
 def home():
+    coordenadas[ponto] = {"latitude": -6, "longitude": -36}
     return "<h1>Primeiro App dplyado no Render</h1>"
 
 @app.route("/coordenadas", methods=["GET"])
 def coordenadas():
-    return jsonify({"latitude": -6, "longitude": -36})
+    ponto = 2
+    coordenadas[ponto] = {"latitude": -6.423, "longitude": -36.1245}
+    return jsonify(coordenadas)
 
 if __name__ == "__main__":
     app.run()
