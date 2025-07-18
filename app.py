a2fdaeef8cafcf192a3fa7cab78385f4c9b1004c
Manoel Ívisson <manoelivisson4@hotmail.com>
2025-07-18 17:15:00 -0300

    Adicionando função de limpar coordenadas do dicionário

--- app.py
+++ app.py
@@ -1,20 +1,36 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-coordenadas = {}
-ponto = 1
+coordenadas_dict = {}
+ponto = 0
 
 @app.route("/", methods=["GET"])
 def home():
-    coordenadas[ponto] = {"latitude": -6, "longitude": -36}
-    return "<h1>Primeiro App dplyado no Render</h1>"
+    return "<h1>Primeiro App deployado no Render</h1>"
 
 @app.route("/coordenadas", methods=["GET"])
 def coordenadas():
-    ponto = 2
-    coordenadas[ponto] = {"latitude": -6.423, "longitude": -36.1245}
-    return jsonify(coordenadas)
+    global ponto
+    ponto += 1
+    coordenadas_dict[ponto] = {"latitude": -6.423, "longitude": -36.1245}
+    return jsonify(coordenadas_dict)
+
+@app.route("/limpar", methods=["GET"])
+def limpar():
+    global coordenadas_dict, ponto
+    coordenadas_dict = {}
+    ponto = 0
+    return "Dicionário zerado"
+
+@app.route("/novas-coordenadas", methods=["POST"])
+def novas_coordenadas():
+    dados = request.get_json()
+
+    latitude = dados.get("latitude")
+    longitude = dados.get("longitude")
+
+    return jsonify({"messagem": "Dados recebidos", "latitude": latitude, "longitude": longitude}), 200
 
 if __name__ == "__main__":
     app.run()
